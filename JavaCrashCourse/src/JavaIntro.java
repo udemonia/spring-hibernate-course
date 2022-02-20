4538601f41b2e5c9813dbaa7f35a19fc2995aea4
udemonia <brandon.l.lambert@icloud.com>
2022-02-19 20:24:19 -0600

    java crash course started

---
+++ JavaCrashCourse/src/JavaIntro.java
@@ -0,0 +1,9 @@
+public class JavaIntro {
+    public static void main(String[] args) {
+        System.out.println("Kalvin is the Best! Yes!");
+
+        int myAge = 39;
+
+
+    }
+}
---
+++ basic-java-primer/section1/HelloJava.java
@@ -0,0 +1,8 @@
+
+public class HelloJava {
+    // * we need to define a main method to run the program
+    // * the main method is the entry point of the program
+    public static void main(String[] args) {
+        System.out.println("Kalvin is the BEST!!");
+    }
+}
